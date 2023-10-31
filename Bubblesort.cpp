4ce542ee57f372279fbb2c1d92448404b582531f
EvanSanjoe <144771585+EvanSanjoe@users.noreply.github.com>
2023-10-31 16:52:02 +0530

    Add files via upload

---
+++ Bubblesort.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+	int j,n,t,i,b,c;
+	printf("Enter number of elements");
+	scanf("%d",&n);
+	int a[n];
+	printf("Enter the elements");
+	for(b=0;b<n;b++)
+	{
+		scanf("%d",&a[b]);
+	}
+	for(i=0;i<n-1;i++)
+	{
+		for(j=0;j<n-i-1;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				t=a[j];
+				a[j]=a[j+1];
+				a[j+1]=t;
+			}
+		}
+	}
+	for(c=0;c<n;c++)
+	{
+		printf("%d",a[c]);
+	}
+}
